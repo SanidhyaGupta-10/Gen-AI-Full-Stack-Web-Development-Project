7b03fc201104ba9c9f21d5f1ed3c57d4dc9a3d16
Sanidhya Gupta <adityasgupta1857@gmail.com>
2026-03-01 00:27:33 +0530

    feat: Initialize server with Express, Mongoose, and Bun setup.

--- server/index.ts
+++ server/index.ts
@@ -1,8 +1,10 @@
 import app from "./src/app";
 import "dotenv/config";
+import connectDB from "./src/config/db";
 
 const PORT = process.env.PORT! || 3000;
 
-app.listen(PORT, () => {
+app.listen(PORT, async () => {
+    await connectDB();
     console.log(`Server is running on port ${PORT}`);
 });
---
+++ server/src/config/db.ts
@@ -0,0 +1,17 @@
+import mongoose from "mongoose";
+
+const MONGODB_URI = process.env.MONGODB_URI;
+
+const connectDB = async () => {
+    try {
+        if(!MONGODB_URI){
+            throw new Error("MONGODB_URI is not defined");
+        }
+        await mongoose.connect(MONGODB_URI);
+        console.log("MongoDB connected");
+    } catch (error) {
+        console.error("Error connecting to MongoDB:", error);
+    }
+};
+
+export default connectDB;
