b8e832c6313e688fdfd12843d1736e6b8bda65fc
Sanidhya Gupta <adityasgupta1857@gmail.com>
2026-03-01 00:08:15 +0530

    Server Setup and Running with Bun because of type safety with TypeScript

---
+++ server/src/app.ts
@@ -0,0 +1,13 @@
+import express from "express";
+import type { Request, Response } from "express";
+
+const app = express();
+
+app.get("/", (req: Request, res: Response) => {
+    res.json({
+        message: "Hello World!"
+    })
+});
+
+
+export default app;
